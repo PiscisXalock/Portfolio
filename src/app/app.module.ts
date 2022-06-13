98fa11345559a62c073bccd396e67d8de8a37d62
Piscis Xalock <jabadg99@gmail.com>
2022-06-13 20:29:17 +0200

    Changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { AboutComponent } from './components/about/about.component';
+import { SkillsComponent } from './components/skills/skills.component';
+import { GamesComponent } from './components/games/games.component';
+import { ContactComponent } from './components/contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    AboutComponent,
+    SkillsComponent,
+    GamesComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
